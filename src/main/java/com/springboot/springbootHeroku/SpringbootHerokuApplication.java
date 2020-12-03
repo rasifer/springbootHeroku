5b8716a2b607ac77cafd4ad3442756fb6142ece3
Rafael Fernandes <rasifer@gmail.com>
2020-12-03 11:03:51 -0300

    Inclusão da página inicial.

--- src/main/java/com/springboot/springbootHeroku/SpringbootHerokuApplication.java
+++ src/main/java/com/springboot/springbootHeroku/SpringbootHerokuApplication.java
@@ -2,12 +2,18 @@ package com.springboot.springbootHeroku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class SpringbootHerokuApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootHerokuApplication.class, args);
+	}
+	
+	@RequestMapping("/home")
+	public String index() {
+		return "index";
 	}
 
 }
