1d472aa5fa15b192699b1eeeb08ab88dd961cbeb
Rafael Fernandes <rasifer@gmail.com>
2020-12-03 10:37:22 -0300

    Inclusão sprintboot no projeto.

---
+++ src/main/java/com/springboot/springbootHeroku/SpringbootHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootHeroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHerokuApplication.class, args);
+	}
+
+}
